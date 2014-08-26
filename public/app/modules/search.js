b09cbc06200018384657d9feb6db9a7fce3d1252
Jorin Vogel <jorin.vogel@gmail.com>
2014-08-27 03:27:18 +0700

    filter on focus select all

--- public/app/modules/search.js
+++ public/app/modules/search.js
@@ -26,12 +26,21 @@
   }
 
 
-  el.addEventListener('focus', app.aside.focus);
+  el.addEventListener('focus', function (e) {
+    app.aside.focus();
+
+    setTimeout(function () {
+      el.select();
+    }, 0);
+  });
+
   el.addEventListener('blur', app.aside.blur);
 
   el.addEventListener('keyup', function (e) {
     // enter or escape
-    if (e.keyCode == 13 || e.keyCode == 27) el.blur();
+    if (e.keyCode == 13 || e.keyCode == 27) {
+      el.blur();
+    }
   });
 
   el.addEventListener('input', function () {
